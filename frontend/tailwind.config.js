690542e2350bd66ce82d6cfdb6dd198ad7989cd0
He Zhu <hzhu2202@uni.sydney.edu.au>
2024-03-06 03:12:27 +1100

    fea: add theme color

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'sea-green': {
+					50: '#f1f8f3',
+					100: '#ddeedf',
+					200: '#bdddc3',
+					300: '#91c49e',
+					400: '#62a575',
+					500: '#45925d',
+					600: '#2f6c43',
+					700: '#255737',
+					800: '#20452e',
+					900: '#1b3926',
+					950: '#0e2016',
+				},
+			},
+		},
 	},
 	plugins: [],
 };
