ccbe2293d4fb355bfc2c34e51f3db710fe9aa1b8
He Zhu <hzhu2202@uni.sydney.edu.au>
2024-03-08 01:45:33 +1100

    fix: update authMiddleware by structured way to throw error

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '../config.js';
 import { User } from '../models/userModel.js';
+import HttpError from '../models/errorModel.js';
 
 const protect = async (req, res, next) => {
 	let token;
@@ -15,19 +16,19 @@ const protect = async (req, res, next) => {
 
 			// Verify token
 			const decoded = jwt.verify(token, JWT_SECRET);
-
+			console.log('.....decoded', decoded);
 			// Get user from the token (you might want to exclude the password when fetching user)
 			req.user = await User.findById(decoded.id).select('-password');
 
 			next();
 		} catch (error) {
 			console.error(error);
-			res.status(401).json({ message: 'Not authorized, token failed' });
+			return next(new HttpError('Not authorized, no token', 401));
 		}
 	}
 
 	if (!token) {
-		res.status(401).json({ message: 'Not authorized, no token' });
+		return next(new HttpError('Not authorized, token failed', 401));
 	}
 };
 
