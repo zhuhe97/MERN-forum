91f672d0d3ec743655cea3fb52db525033046bfe
He Zhu <hzhu2202@uni.sydney.edu.au>
2024-04-01 23:24:21 +1100

    refactor: add postprovider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext.jsx';
+import { PostProvider } from './context/PostContext.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
@@ -8,26 +9,25 @@ import DeletePost from './pages/DeletePost';
 import EditPost from './pages/EditPost';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Profile from './components/Profile';
+import Profile from './pages/Profile';
 
 const App = () => {
-	const handleLogin = userData => {
-		localStorage.setItem('token', userData.token);
-	};
 	return (
 		<AuthProvider>
-			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/register' element={<Register />} />
-				<Route path='/login' element={<Login onLogin={handleLogin} />} />
-				<Route element={<ProtectedRoute />}>
-					<Route path='/profile' element={<Profile />} />
-					<Route path='/posts/create' element={<CreatePost />} />
+			<PostProvider>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/register' element={<Register />} />
+					<Route path='/login' element={<Login />} />
 					<Route path='/posts/details/:id' element={<ShowPost />} />
-					<Route path='/posts/edit/:id' element={<EditPost />} />
-					<Route path='/posts/delete/:id' element={<DeletePost />} />
-				</Route>
-			</Routes>
+					<Route element={<ProtectedRoute />}>
+						<Route path='/profile' element={<Profile />} />
+						<Route path='/posts/create' element={<CreatePost />} />
+						<Route path='/posts/edit/:id' element={<EditPost />} />
+						<Route path='/posts/delete/:id' element={<DeletePost />} />
+					</Route>
+				</Routes>
+			</PostProvider>
 		</AuthProvider>
 	);
 };
