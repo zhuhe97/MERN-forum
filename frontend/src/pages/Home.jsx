747a91a691523281ae98f1b07368afb7d0e6a02b
He Zhu <hzhu2202@uni.sydney.edu.au>
2024-04-02 21:15:25 +1100

    refactor: refactor home page and add postTable component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,87 +1,36 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import Spinner from '../components/Spinner';
 import { useNavigate } from 'react-router-dom';
 import { MdOutlineAddBox } from 'react-icons/md';
-import PostsCard from '../components/home/PostsCard';
 import PostsTable from '../components/home/PostsTable';
-import { useAuth } from '../utils/auth';
 import Header from '../components/Header';
+import { useAuth } from '../context/AuthContext';
 
 const Home = () => {
-	const [posts, setPosts] = useState([]);
-	const [loading, setLoading] = useState(false);
-	const [showType, setShowType] = useState('table');
-	const [currentUser, setCurrentUser] = useState(null);
-
-	const { validateTokenAndRedirect, getUser } = useAuth();
+	const { isAuthenticated } = useAuth();
 	const navigate = useNavigate();
 
 	const handleCreatePostClick = () => {
-		if (validateTokenAndRedirect()) {
+		if (isAuthenticated()) {
 			navigate('/posts/create');
+		} else {
+			console.log('isAuthenticated == false');
+			sessionStorage.setItem('post-login-redirect', '/posts/create');
+			navigate('/login');
 		}
 	};
-
-	const handleLogout = () => {
-		localStorage.removeItem('token');
-		navigate('/login');
-	};
-
-	useEffect(() => {
-		setLoading(true);
-		setCurrentUser(getUser());
-		axios
-			.get('http://localhost:5555/posts')
-			.then(response => {
-				setPosts(response.data.data);
-				setLoading(false);
-			})
-			.catch(error => {
-				console.log(error);
-				setLoading(false);
-			});
-	}, [getUser]);
-
 	return (
-		<div>
+		<div className='bg-sea-green-50 min-h-screen relative pt-16'>
+			{' '}
+			{/* pt-16 to offset for fixed header */}
 			<Header />
-			<div className='p-4'>
-				<div className='flex justify-center items-center gap-x-4'>
-					<button
-						className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-1g'
-						onClick={() => setShowType('table')}
-					>
-						Table
-					</button>
-					<button
-						className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-1g'
-						onClick={() => setShowType('card')}
-					>
-						Card
-					</button>
+			<div className='container mx-auto px-4 py-8'>
+				<div className='mb-4 flex justify-end'>
+					<MdOutlineAddBox
+						className='text-sea-green-600 hover:text-sea-green-800 cursor-pointer text-5xl'
+						onClick={handleCreatePostClick}
+						title='Create New Post'
+					/>
 				</div>
-				<button
-					className='bg-red-500 hover:bg-red-700 px-4 py-1 rounded-1g'
-					onClick={handleLogout}
-				>
-					Logout
-				</button>
-				<div className='flex justify-between items-center'></div>
-				<h1 className='text-3xl my-8'>Posts List</h1>
-
-				<MdOutlineAddBox
-					className='text-sky-800 text-4xl'
-					onClick={handleCreatePostClick}
-				/>
-
-				{loading ? (
-					<Spinner />
-				) : showType === 'table' ? (
-					<PostsTable posts={posts} currentUser={currentUser} />
-				) : (
-					<PostsCard posts={posts} currentUser={currentUser} />
-				)}
+				<PostsTable />
 			</div>
 		</div>
 	);
