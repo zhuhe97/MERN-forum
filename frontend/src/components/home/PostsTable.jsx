d799a8f26e0eec2c65adb5de20d0b0587ae0f9a6
He Zhu <hzhu2202@uni.sydney.edu.au>
2024-04-02 21:16:01 +1100

    refactor: refactor post component' UI

--- frontend/src/components/home/PostsTable.jsx
+++ frontend/src/components/home/PostsTable.jsx
@@ -1,66 +1,98 @@
-import { Link } from 'react-router-dom';
-import { AiOutlineEdit } from 'react-icons/ai';
-import { BsInfoCircle } from 'react-icons/bs';
-import { MdOutlineDelete } from 'react-icons/md';
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import Pagination from './Pagination';
+import axios from 'axios';
 
-function PostsTable({ posts, currentUser }) {
+function PostsTable() {
+	const [posts, setPosts] = useState([]);
+	const [loading, setLoading] = useState(false);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [totalPages, setTotalPages] = useState(0);
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		setLoading(true);
+		axios
+			.get(`http://localhost:5555/api/v1/posts?page=${currentPage}`)
+			.then(response => {
+				setPosts(response.data.data.data);
+				setTotalPages(response.data.data.totalPages);
+				setLoading(false);
+			})
+			.catch(error => {
+				console.error(error);
+				setLoading(false);
+			});
+	}, [currentPage]);
+
+	const handlePageChange = page => {
+		setCurrentPage(page);
+	};
+
+	if (loading) {
+		return <div className='text-center'>Loading...</div>;
+	}
+
 	// Function to truncate post content
 	const truncateContent = (content, charLimit = 100) => {
-		if (typeof content !== 'string') {
-			console.error('Content is not a string:', content);
-			return ''; // or return a default message
-		}
-
 		if (content.length > charLimit) {
 			return content.substring(0, charLimit) + '...';
 		}
 		return content;
 	};
 
+	// Function to format date
+	const formatDate = dateString => {
+		return new Date(dateString).toLocaleDateString();
+	};
+
+	// Handle row click
+	const handleRowClick = postId => {
+		navigate(`/posts/details/${postId}`);
+	};
+
 	return (
-		<table className='w-full border-separate border-spacing-2'>
-			<thead>
-				<tr>
-					<th className='border norder-slate-600 rounded-md'> No</th>
-					<th className='border norder-slate-600 rounded-md'> Title</th>
-					<th className='border norder-slate-600 rounded-md'> Content</th>
-					<th className='border norder-slate-600 rounded-md'> Operations</th>
-				</tr>
-			</thead>
-			<tbody>
-				{posts.map((post, index) => (
-					<tr key={post.id} className='h-8'>
-						<td className='border norder-slate-700 rounded-md text-center'>
-							{index + 1}
-						</td>
-						<td className='border norder-slate-700 rounded-md text-center'>
-							{post.title}
-						</td>
-						<td className='border norder-slate-700 rounded-md text-center'>
-							{truncateContent(post.content)}
-						</td>
-						<td className='border norder-slate-700 rounded-md text-center'>
-							<div className='flex justify-center gap-x-4'>
-								<Link to={`/posts/details/${post._id}`}>
-									<BsInfoCircle className='text-2xl text-green-800' />
-								</Link>
-								{/* Render Edit and Delete buttons only if currentUser is the creator of the post */}
-								{currentUser && currentUser.id === post.user && (
-									<>
-										<Link to={`/posts/edit/${post._id}`}>
-											<AiOutlineEdit className='text-2xl text-yellow-600' />
-										</Link>
-										<Link to={`/posts/delete/${post._id}`}>
-											<MdOutlineDelete className='text-2xl text-red-600' />
-										</Link>
-									</>
-								)}
-							</div>
-						</td>
+		<div className='max-w-5xl mx-auto my-8 bg-white shadow rounded-lg overflow-hidden'>
+			<table className='w-full table-auto'>
+				<thead>
+					<tr className='text-xs font-semibold uppercase text-gray-400 bg-gray-50'>
+						<th className='px-2 py-2'>Title</th>
+						<th className='px-2 py-2'>Author</th>
+						<th className='px-2 py-2'>Content</th>
+						<th className='px-2 py-2'>Replies</th>
+						<th className='px-2 py-2'>Last Response</th>
 					</tr>
-				))}
-			</tbody>
-		</table>
+				</thead>
+				<tbody className='text-xs text-gray-600'>
+					{posts.map(post => (
+						<tr
+							key={post.id}
+							className='hover:bg-gray-100 cursor-pointer'
+							onClick={() => handleRowClick(post._id)}
+						>
+							<td className='px-2 py-1'>{truncateContent(post.title, 50)}</td>
+							<td className='px-2 py-1 flex items-center'>
+								<img
+									src={post.user.avatar || 'default-avatar-url'}
+									alt='Avatar'
+									className='w-6 h-6 rounded-full mr-2'
+								/>
+								{post.user.username || 'Author Name'}
+							</td>
+							<td className='px-2 py-1'>{truncateContent(post.content, 50)}</td>
+							<td className='px-2 py-1'>{post.commentsCount}</td>
+							<td className='px-2 py-1'>{formatDate(post.createdAt)}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+
+			<Pagination
+				currentPage={currentPage}
+				totalPages={totalPages}
+				onPageChange={handlePageChange}
+			/>
+		</div>
 	);
 }
 
